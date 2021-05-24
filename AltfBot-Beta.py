0ee540a6d4e690983c6cff203310cdbbffef6402
Linus <linusaltacc01@protonmail.com>
2021-05-24 18:55:34 +0530

    fixed :45 bug

--- AltfBot-Beta.py
+++ AltfBot-Beta.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import xlrd
 import os
 
-TOKEN = os.environ["TOKEN"]
+TOKEN = '1898176973:AAE8RkDDEXaeTyPzrGKNTdJd6MVxlv1BAt8'
 from datetime import datetime, time, date
 #TOKEN = None
 
@@ -32,11 +32,11 @@ def enna(update, context):
     try:
         if context.args[0] == "class":
             ########Code for schedule
-	   # import xlrd
+	        # import xlrd
             # To open Workbook
             wb = xlrd.open_workbook('classSchedulle.xls')
             sheet = wb.sheet_by_index(0)
-            #Monday, Tuesday, Wednesday, Thursday, Friday, Saturday = 1, 2, 3, 4, 5, 6
+            # Monday, Tuesday, Wednesday, Thursday, Friday, Saturday = 1, 2, 3, 4, 5, 6
             from datetime import time, datetime, date
             now = datetime.now()
             hour = int(now.strftime("%H"))
@@ -64,45 +64,43 @@ def enna(update, context):
                 temp = "Sunday"
                 session = 'inaiku Leave uh'
             #example for understanding ::print(sheet.cell_value(monday, 8))
-            if minute<45:
-                if temp != "Sunday":
-                    if hour<9:
-                        session = "Class usually starts at 9 AM"
-                    elif hour == 9:
-                        session = sheet.cell_value(dayy, 1)
-                    elif hour == 10:
-                        session = sheet.cell_value(dayy, 2)
-                    elif hour == 11:
-                        session = sheet.cell_value(dayy, 3)
-                    elif hour == 12:
-                        session = sheet.cell_value(dayy, 4)
-                    elif hour == 13:
-                        session = "Lunch time! Go eat!"
-                    elif hour == 14:
-                        session = sheet.cell_value(dayy, 6)
-                    elif hour == 15:
-                        session = sheet.cell_value(dayy, 7)
-                    elif hour == 16:
-                        session = sheet.cell_value(dayy, 8)
-                    elif hour>16:
-                        session = '5 mani mela class irukathu (mostly)'
-                    
-                    ########
-                elif context.args[0] == "date":
-                    from datetime import date, datetime
-                    date = datetime.now().strftime("%d-%m-%Y")
-                    #context.bot.send_message(chat_id=update.effective_chat.id, text=str(datentime))
-                    session = str(date)
-                elif context.args[0] == "time":
-                    from datetime import datetime,time
-                    now = datetime.now()
-                    time = now.strftime("%H : %M") 
-                    #timestamp = datetime.now().strftime('%H:%M:%S.%f')
-                    session = str(time)
-                else:
-                    session = "puriyala"
-            else:
-                session = "Last class has been ended"
+            if temp != "Sunday":
+                if hour<9:
+                    session = "Class usually starts at 9 AM"
+                elif hour == 9:
+                    session = sheet.cell_value(dayy, 1)
+                elif hour == 10:
+                    session = sheet.cell_value(dayy, 2)
+                elif hour == 11:
+                    session = sheet.cell_value(dayy, 3)
+                elif hour == 12:
+                    session = sheet.cell_value(dayy, 4)
+                elif hour == 13:
+                    session = "Lunch time! Go eat!"
+                elif hour == 14:
+                    session = sheet.cell_value(dayy, 6)
+                elif hour == 15:
+                    session = sheet.cell_value(dayy, 7)
+                elif hour == 16:
+                    session = sheet.cell_value(dayy, 8)
+                elif hour>16:
+                    session = '5 mani mela class irukathu (mostly)'
+                if minute>45 and hour<15:
+                    session = session + " session has ended at " + str(hour) + ":" + str(minute)
+            ########
+        elif context.args[0] == "date":
+            from datetime import date, datetime
+            date = datetime.now().strftime("%d-%m-%Y")
+            #context.bot.send_message(chat_id=update.effective_chat.id, text=str(datentime))
+            session = str(date)
+        elif context.args[0] == "time":
+            from datetime import datetime,time
+            now = datetime.now()
+            time = now.strftime("%H : %M") 
+            #timestamp = datetime.now().strftime('%H:%M:%S.%f')
+            session = str(time)
+        else:
+            session = "puriyala"
     except (IndexError, ValueError):
         context.bot.send_message(chat_id=update.effective_chat.id, text="/enna Command Arguments: \n\t1. class\n\t2. date\n\t3. time")
         session = "/enna command ku arguments onume pass panala!"
