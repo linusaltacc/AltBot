09a45bfe9b3a0c910829f0655cafcae8bbae27dc
Sunil <70189009+linusaltacc@users.noreply.github.com>
2021-05-28 12:02:18 +0530

    Changed variables to call datetime from inside the function to update everytime command is called

--- modules/DateAndTime.py
+++ modules/DateAndTime.py
@@ -1,17 +1,21 @@
 from datetime import datetime, date, time
-date = date.today()
-now = datetime.now()
+
 def Time():
+    now = datetime.now()
     return now.strftime("%I : %M %p") 
     #timestamp = datetime.now().strftime('%H:%M:%S.%f')
 def HourAs12():
+    now = datetime.now()
     return str(now.strftime("%I"))
 def HourAs24():
+    now = datetime.now()
     return int(now.strftime("%H"))
 def Minute():
+    now = datetime.now()
     return int(now.strftime("%M"))
 def Date():
     return str(datetime.now().strftime("%d-%m-%Y"))
 def Day():
+    date = date.today()
     return str(date.strftime("%A"))
 #print(Time(), HourAs12(),HourAs24(), Minute(),Date(),Day())
